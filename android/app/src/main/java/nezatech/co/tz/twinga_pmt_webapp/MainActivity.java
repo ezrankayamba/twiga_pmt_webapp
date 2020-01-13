6144501265be547d16eeb251f2cc5c66534d4479
ezrankayamba <ezrankayamba@gmail.com>
2020-01-13 08:23:46 +0300

    Initial

---
+++ android/app/src/main/java/nezatech/co/tz/twinga_pmt_webapp/MainActivity.java
@@ -0,0 +1,13 @@
+package nezatech.co.tz.twinga_pmt_webapp;
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity;
+import io.flutter.embedding.engine.FlutterEngine;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  public void configureFlutterEngine(@NonNull FlutterEngine flutterEngine) {
+    GeneratedPluginRegistrant.registerWith(flutterEngine);
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,56 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_webview_plugin/flutter_webview_plugin.dart';
+import 'package:permission_handler/permission_handler.dart';
+
+void main() => runApp(PMTWebView());
+
+class PMTWebView extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _PMTWebViewState();
+  }
+}
+
+class _PMTWebViewState extends State<PMTWebView> {
+  checkPermissions() async {
+    PermissionStatus pStatus = await PermissionHandler()
+        .checkPermissionStatus(PermissionGroup.location);
+
+    if(pStatus!=PermissionStatus.granted){
+      debugPrint("Permission Status: " + pStatus.toString());
+      Map<PermissionGroup, PermissionStatus> permissions = await PermissionHandler().requestPermissions([PermissionGroup.location]);
+      debugPrint("Permission Status2: " + permissions.toString());
+    }else{
+      debugPrint("Permission Status: " + pStatus.toString());
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    checkPermissions();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          primaryColor: Color(0xff2b7a78),
+        ),
+        title: 'Twiga PMT App',
+        home: WebviewScaffold(
+          appBar: PreferredSize(
+            child: Container(),
+            preferredSize: Size.fromHeight(0.0),
+          ),
+          url: "https://pmt.nezatech.co.tz/",
+          geolocationEnabled: true,
+          supportMultipleWindows: true,
+          withJavascript: true,
+          debuggingEnabled: true,
+        ));
+  }
+}
